615cc011e5e0db8a7cfb546e2ef8bd91be04c245
ChanIl Kang <programmer.chanil@gmail.com>
2022-06-13 00:41:08 +0900

    create a player
    create a player

---
+++ index.js
@@ -0,0 +1,33 @@
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    draw() {
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+        context.fillStyle = this.color;
+        context.fill();
+    }
+}
+
+const x = canvas.width / 2;
+const y = canvas.height / 2;
+const player = new Player(x, y, 30, 'blue');
+
+
+function main() {
+    player.draw();
+    console.log(player);
+}
+
+main();
