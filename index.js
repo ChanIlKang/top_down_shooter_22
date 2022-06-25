bfa1d776bd16539d325314e62a8be6e770946a02
ChanIl Kang <programmer.chanil@gmail.com>
2022-06-25 14:06:42 +0900

    Update 4th
    Detect collision between enemies and player.
    Remove projectiles from edges of screen
    :heavy_plus_sign: Give random color for enemies.

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ class Enemy {
 
 const x = canvas.width / 2;
 const y = canvas.height / 2;
-const player = new Player(x, y, 30, 'blue');
+const player = new Player(x, y, 10, 'white');
 const projectiles = [];
 const enemies = [];
 
@@ -89,7 +89,7 @@ function spawnEnemies() {
             x = Math.random() * canvas.width;
             y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
         }
-        const color = 'green';
+        const color = `hsl(${Math.random() * 360}, 50%, 50%)`;
 
         const angle = Math.atan2(
             canvas.height / 2 - y,
@@ -101,21 +101,51 @@ function spawnEnemies() {
             y: Math.sin(angle)
         }
         enemies.push(new Enemy(x, y, radius, color, velocity));
-
-
     }, 1000);
 }
 
+let animationId;
+
 function animate() {
-    requestAnimationFrame(animate);
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    animationId = requestAnimationFrame(animate);
+    context.fillStyle = 'rgba(0, 0, 0, 0.1)';
+    context.fillRect(0, 0, canvas.width, canvas.height);
     player.draw();
-    projectiles.forEach((projectile) => {
+    projectiles.forEach((projectile, index) => {
         projectile.update();
+
+        //remove projectile from edges of screen
+        if (projectile.x - projectile.radius < 0 ||
+            projectile.x - projectile.radius > canvas.width ||
+            projectile.y + projectile.radius < 0 ||
+            projectile.y - projectile.radius > canvas.height) {
+
+            setTimeout(() => {
+                projectiles.splice(index, 1);
+            }, 0);
+        }
     })
 
-    enemies.forEach((enemy) => {
+    enemies.forEach((enemy, index) => {
         enemy.update();
+
+        const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y);
+
+        //end game
+        if (dist - enemy.radius - player.radius < 1) {
+            cancelAnimationFrame(animationId);
+        }
+
+        projectiles.forEach((projectile, projectileIndex) => {
+            const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
+            //objects touch
+            if (dist - enemy.radius - projectile.radius < 1) {
+                setTimeout(() => {
+                    enemies.splice(index, 1);
+                    projectiles.splice(projectileIndex, 1);
+                }, 0);
+            }
+        })
     })
 }
 
@@ -126,15 +156,15 @@ addEventListener('click', (event) => {
     )
 
     const velocity = {
-        x: Math.cos(angle),
-        y: Math.sin(angle)
+        x: Math.cos(angle) * 5.3,
+        y: Math.sin(angle) * 5.3
     }
 
     projectiles.push(new Projectile(
         canvas.width / 2,
         canvas.height / 2,
         5,
-        'red',
+        'white',
         velocity)
     )
 })
